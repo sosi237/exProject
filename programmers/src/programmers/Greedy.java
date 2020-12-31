b0dcd05c2f9290bb0ff8743300aa10ca4ae9f65e
soyeon <lupmika@naver.com>
2020-12-31 11:56:55 +0900

    for each문 사용

--- programmers/src/programmers/Greedy.java
+++ programmers/src/programmers/Greedy.java
@@ -13,12 +13,17 @@ public class Greedy {
 	}
 	public static int getCount(int N) {
 		int cnt = 0;
-			
 		int[] arrCoin = {500, 100, 50, 10};
+		for(int coin : arrCoin) {
+			cnt += N / coin;
+			N %= coin;
+		}
+		/*	
 		for(int i = 0; i < arrCoin.length; i++) {
 			cnt += N / arrCoin[i];
 			N %= arrCoin[i];
 		}
+		*/
 		
 		return cnt;
 	}
