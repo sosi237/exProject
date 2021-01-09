1b800f1200ebd28ddbf6375045d4ff8940ac56f0
soyeon <lupmika@naver.com>
2021-01-09 23:09:11 +0900

    test

--- programmers/src/programmers/ToMakeOne.java
+++ programmers/src/programmers/ToMakeOne.java
@@ -1,5 +1,4 @@
 package programmers;
-
 /*
  * 어떠한 수 N이 1이 될때까지 다음의 두 과정 중 하나를 반복적으로 선택하여 수행하려 한다.
  * 단, 두 번째 연산은 N이 K로 나누어떨어질 때만 선택할 수 있다.
@@ -25,7 +24,6 @@ public class ToMakeOne {
 			}
 			cnt++;
 		}
-		
 		return cnt;
 	}
 }
